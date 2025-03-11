b534f30834fd73ed4abe378314c3f1893296681a
yussufnarcos <narcosyussuf@gmail.com>
2025-03-11 15:20:35 +0300

    phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+}
+    var bestCustomer; 
+
+    function setBestCustomer() {
+      bestCustomer = 'not bob';
+    }
+    
+    function overwriteBestCustomer() {
+      bestCustomer = 'maybe bob';
+    }
+    
+
+
+const leastFavoriteCustomer='hassan';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='yussuf';
+}
